155b4f0e617f847dd046a6daf66321bb656eed1c
Katharine Murphy <niramisa777kubera@gmail.com>
2021-09-14 16:15:25 +0200

    first commit

---
+++ ex1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/* this is a comment */
+int main(int argc, char *argv[])
+{
+	int distance = 100;
+	int total = 1000;
+
+	// this is also a comment
+	printf("You are %d miles away from %d things.\n", distance, total);
+
+	return 0;
+}
