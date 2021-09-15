43eb0edca0b0b3bc3874eac63bd36d0338f947fc
Katharine Murphy <niramisa777kubera@gmail.com>
2021-09-15 12:43:30 +0200

    adding new exercises

---
+++ ex8.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	if (argc == 1) 
+	{
+		printf("You have no arguments.\n");
+	}
+	else if (argc == 2)
+	{
+		printf("You only have one argument.\n");
+	}
+	else if (argc > 2 && argc < 5)
+	{
+		printf("Here's your arguments:\n");
+
+		for (i = 0; i < argc; i++) 
+		{
+			printf("%s ", argv[i]);
+		}
+		
+		printf("\n");
+	}
+	else
+	{
+		printf("You have too many arguments.\n");
+	}
+
+	return 0;
+
+}
